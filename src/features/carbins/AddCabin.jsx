a3d5011d5ef034cc9f8ae99b411403d705de9d7d
yushu <zhuysh18@163.com>
2025-04-24 15:28:28 +1000

    convert the Modal to a compound component

--- src/features/carbins/AddCabin.jsx
+++ src/features/carbins/AddCabin.jsx
@@ -1,9 +1,29 @@
-import { useState } from "react";
 import Button from "../../ui/Button";
 import Modal from "../../ui/Modal";
 import CreateCabinForm from "./CreateCabinForm";
+import CabinTable from "./CabinTable";
 
 function AddCabin() {
+  return (
+    <Modal>
+      <Modal.Open opens="cabin-form">
+        <Button>Add new cabin</Button>
+      </Modal.Open>
+      <Modal.Window name="cabin-form">
+        <CreateCabinForm />
+      </Modal.Window>
+
+      <Modal.Open opens="table">
+        <Button>Show table</Button>
+      </Modal.Open>
+      <Modal.Window name="table">
+        <CabinTable />
+      </Modal.Window>
+    </Modal>
+  );
+}
+
+/* function AddCabin() {
   const [isOpenModal, setIsOpenModal] = useState(false);
   return (
     <div className="">
@@ -19,6 +39,6 @@ function AddCabin() {
       )}
     </div>
   );
-}
+} */
 
 export default AddCabin;
